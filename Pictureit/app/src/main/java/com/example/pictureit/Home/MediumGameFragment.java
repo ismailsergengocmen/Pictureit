5c0c356c7b16512a4d50fd061baf17b21d27cba1
cemalfarukguney <cemalfarukguney@users.noreply.github.com>
2020-05-17 01:58:53 +0300

    Game Fragments and Home Activity
    Fragments now check the database for photos.

--- Pictureit/app/src/main/java/com/example/pictureit/Home/MediumGameFragment.java
+++ Pictureit/app/src/main/java/com/example/pictureit/Home/MediumGameFragment.java
@@ -1,10 +1,13 @@
 package com.example.pictureit.Home;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 
@@ -15,20 +18,83 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.pictureit.R;
 import com.example.pictureit.Utils.ImageAdapter;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.pictureit.R;
+import com.example.pictureit.Utils.BottomNavigationViewHelper;
+import com.example.pictureit.Utils.SectionsPagerAdapter;
+import com.example.pictureit.Utils.UniversalImageLoader;
+import com.example.pictureit.models.Photo;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import android.accounts.Account;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.util.ArrayList;
 
 public class MediumGameFragment extends Fragment {
 
-    private static final String TAG = "MediumGameFragment";
+    private static final String TAG = "EasyGameFragment";
+
+    public interface OnGridImageSelectedListener{
+        void onGridImageSelected(Photo photo, Context context);
+    }
+    OnGridImageSelectedListener mOnGridImageSelectedListener;
+
     GridView gridView;
-    public int[] images = {R.drawable.ic_empty_grid,R.drawable.ic_empty_grid,R.drawable.ic_empty_grid,R.drawable.ic_empty_grid,R.drawable.ic_empty_grid};
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_medium_game, container, false);
         gridView = (GridView) view.findViewById(R.id.grid_view);
-        ImageAdapter adapter = new ImageAdapter(getActivity(), images);
-        gridView.setAdapter(adapter);
+        setupGridView();
+
 
         ImageView backArrow = view.findViewById(R.id.backArrow);
         backArrow.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +105,74 @@ public class MediumGameFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context){
+        try{
+            mOnGridImageSelectedListener = (OnGridImageSelectedListener) getActivity();
+        }catch(ClassCastException e){
+            Log.e(TAG, "onAttach: ClassCastException:  " + e.getMessage());
+        }
+        super.onAttach(context);
+    }
+
+    private static final int NUM_GRID_COLUMNS = 3;
+
+    private void setupGridView(){
+        Log.d(TAG, "setupGridView: Setting up image grid.");
+
+        final ArrayList<Photo> photos = new ArrayList<>();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference
+                .child(getString(R.string.dbname_user_photos))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for ( DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+
+                    Photo photo = new Photo();
+                    Map<String, Object> objectMap = (HashMap<String, Object>) singleSnapshot.getValue();
+
+                    try {
+                        photo.setTags(objectMap.get(getString(R.string.field_tags)).toString());
+                        photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
+                        photo.setDate_created(objectMap.get(getString(R.string.field_date_created)).toString());
+                        photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
+
+                        photos.add(photo);
+                    }catch(NullPointerException e){
+                        Log.e(TAG, "onDataChange: NullPointerException: " + e.getMessage() );
+                    }
+                }
+
+                //setup our image grid
+                int gridWidth = getResources().getDisplayMetrics().widthPixels;
+                int imageWidth = gridWidth/NUM_GRID_COLUMNS;
+                gridView.setColumnWidth(imageWidth);
+
+                ArrayList<String> imgUrls = new ArrayList<String>();
+                for(int i = 0; i < photos.size(); i++){
+                    imgUrls.add(photos.get(i).getImage_path());
+                }
+                ImageAdapter adapter = new ImageAdapter(getActivity(),R.layout.layout_grid_imageview,
+                        "", imgUrls);
+                gridView.setAdapter(adapter);
+
+                gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        mOnGridImageSelectedListener.onGridImageSelected(photos.get(position), getActivity());
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: query cancelled.");
+            }
+        });
     }
 
     public void backToActivity() {
