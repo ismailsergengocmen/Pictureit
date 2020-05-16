418574c743c87c4c5ea10991771a87246a4e04c8
İsmail Sergen Göçmen <ismailsergengocmen@users.noreply.github.com>
2020-05-16 14:34:37 +0300

    Retrieving user data from firebase
    Now, user can see their profile photos and display names in profile fragment. Necessary methods added

--- Pictureit/app/src/main/java/com/example/pictureit/Profile/ProfileFragment.java
+++ Pictureit/app/src/main/java/com/example/pictureit/Profile/ProfileFragment.java
@@ -18,8 +18,21 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.example.pictureit.Login.LoginActivity;
 import com.example.pictureit.R;
 import com.example.pictureit.Utils.BottomNavigationViewHelper;
+import com.example.pictureit.Utils.FirebaseMethods;
+import com.example.pictureit.Utils.UniversalImageLoader;
+import com.example.pictureit.models.User;
+import com.example.pictureit.models.UserAccountSettings;
+import com.example.pictureit.models.UserSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -30,6 +43,12 @@ public class ProfileFragment extends Fragment {
 
     private static final int ACTIVITY_NUMBER = 2;
 
+    //Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseMethods mFirebaseMethods;
     //Widgets
     private TextView mDisplayName, mUserName, mPhotoNumber;
     private ProgressBar mProgressBar;
@@ -52,14 +71,35 @@ public class ProfileFragment extends Fragment {
         profileMenu = view.findViewById(R.id.profileMenu);
         bottomNavigationViewEx = view.findViewById(R.id.bottomNavViewBar);
         mContext = getActivity();
+        mFirebaseMethods = new FirebaseMethods(getActivity());
 
         Log.d(TAG, "onCreateView: started.");
 
         setupBottomNavigationView();
         setupToolBar();
+
+        setupFirebaseAuth();
         return view;
     }
 
+    private void setProfileWidgets(UserSettings userSettings){
+        Log.d(TAG,"setProfileWidgets: setting widgets with data retrieving from firebase database: " + userSettings.toString());
+        Log.d(TAG,"setProfileWidgets: setting widgets with data retrieving from firebase database: " + userSettings.getSettings().getUsername());
+
+        User user= userSettings.getUser();
+        UserAccountSettings settings = userSettings.getSettings();
+
+        UniversalImageLoader.setImage(settings.getProfile_photo(),mProfilePhoto, null ,"");
+
+        mDisplayName.setText(settings.getDisplay_name());
+        mUserName.setText(settings.getUsername());
+        mProgressBar.setVisibility(View.GONE);
+
+    }
+
+    /**
+     * Responsible for setting up that profile toolbar
+     */
     private void setupToolBar() {
         ((ProfileActivity) getActivity()).setSupportActionBar(toolbar);
 
@@ -84,6 +124,67 @@ public class ProfileFragment extends Fragment {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUMBER);
         menuItem.setChecked(true);
+    }
+
+
+//-----------------------------------------Firebase-------------------------------------------------
+
+    /**
+     * Setup the firebase auth object
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+
+                if (user != null) {
+                    //User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in" + user.getUid());
+                } else {
+                    //User is signed out
+                    Log.d(TAG, "on AuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //retrieve user information from the database
+                setProfileWidgets(mFirebaseMethods.getUserSettings(dataSnapshot));
+
+                //retrieve images for the user in question
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
 }
--- Pictureit/app/src/main/java/com/example/pictureit/Utils/FirebaseMethods.java
+++ Pictureit/app/src/main/java/com/example/pictureit/Utils/FirebaseMethods.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 import com.example.pictureit.R;
 import com.example.pictureit.models.User;
 import com.example.pictureit.models.UserAccountSettings;
+import com.example.pictureit.models.UserSettings;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseNetworkException;
@@ -49,13 +50,13 @@ public class FirebaseMethods {
 
         User user = new User();
 
-        for (DataSnapshot ds : datasnapshot.child(userID).getChildren()){
+        for (DataSnapshot ds : datasnapshot.child(userID).getChildren()) {
             Log.d(TAG, "checkIfUsernameExists: datasnapshot: " + ds);
 
             user.setUsername(ds.getValue(User.class).getUsername());
             Log.d(TAG, "checkIfUsernameExists: datasnapshot: " + user.getUsername());
 
-            if(StringManipulation.expandUsername(user.getUsername()).equals(username)){
+            if (StringManipulation.expandUsername(user.getUsername()).equals(username)) {
                 Log.d(TAG, "checkIfUsernameExists: FOUND A MATCH" + user.getUsername());
                 return true;
             }
@@ -93,32 +94,34 @@ public class FirebaseMethods {
                 });
     }
 
-    public void sendVerificationEmail(){
+    public void sendVerificationEmail() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        if(user !=null){
+        if (user != null) {
             user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
 
-                    }else{
+                    } else {
                         Toast.makeText(mContext, "couldn't send verification email.", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
         }
     }
+
     /**
      * Add information to the users nodes
      * Add information to the user_account_setting node
+     *
      * @param email
      * @param username
      * @param description
      * @param website
      * @param profile_photo
      */
-    public void  addNewUser(String email, String username, String description, String website, String profile_photo) {
+    public void addNewUser(String email, String username, String description, String website, String profile_photo) {
 
         User user = new User(userID, 1, email, StringManipulation.condenseUsername(username));
 
@@ -133,11 +136,107 @@ public class FirebaseMethods {
                 0,
                 0,
                 profile_photo,
-                username,
+                StringManipulation.condenseUsername(username),
                 website
         );
         myRef.child(mContext.getString(R.string.dbname_user_account_settings))
                 .child(userID)
                 .setValue(settings);
+    }
+
+    /**
+     * Retrieves the account settings for tech user currently logged in
+     * Database: user_account_settings node
+     *
+     * @param dataSnapshot
+     * @return
+     */
+    public UserSettings getUserSettings(DataSnapshot dataSnapshot) {
+        Log.d(TAG, "getUserAccountSettings: retrieving user account settings from firebase");
+
+        UserAccountSettings settings = new UserAccountSettings();
+        User user = new User();
+
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+            //user_account_settings node
+            if (ds.getKey().equals(mContext.getString(R.string.dbname_user_account_settings))) {
+                Log.d(TAG, "getUserAccountSettings: datasnapshot:" + ds);
+
+                try {
+                    settings.setDisplay_name(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getDisplay_name()
+                    );
+                    settings.setUsername(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getUsername()
+                    );
+                    settings.setWebsite(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getWebsite()
+                    );
+                    settings.setDescription(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getDescription()
+                    );
+                    settings.setProfile_photo(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getProfile_photo()
+                    );
+                    settings.setPosts(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getPosts()
+                    );
+                    settings.setFollowing(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getFollowing()
+                    );
+                    settings.setFollowers(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getFollowers()
+                    );
+                    Log.d(TAG, "getUserAccountSettings: retrieved user_account_settings information:" + settings.toString());
+                } catch (NullPointerException e) {
+                    Log.e(TAG, " getUserAccountSettings : NullPointerException: " + e.getMessage());
+                }
+
+            }
+            //users node
+            if (ds.getKey().equals(mContext.getString(R.string.dbname_users))) {
+                Log.d(TAG, "getUserAccountSettings: datasnapshot:" + ds);
+
+                user.setUsername(
+                        ds.child(userID)
+                                .getValue(User.class)
+                                .getUsername()
+                );
+                user.setEmail(
+                        ds.child(userID)
+                                .getValue(User.class)
+                                .getEmail()
+                );
+                user.setPhone_number(
+                        ds.child(userID)
+                                .getValue(User.class)
+                                .getPhone_number()
+                );
+                user.setUser_id(
+                        ds.child(userID)
+                                .getValue(User.class)
+                                .getUser_id()
+                );
+                Log.d(TAG, "getUserAccountSettings : retrieved users information : " + user.toString());
+            }
+        }
+        return new UserSettings(user,settings);
     }
 }
