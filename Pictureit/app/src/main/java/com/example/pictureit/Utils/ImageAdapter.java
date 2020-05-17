03c394c3d09c93d0646e5f789199fe2fcdfbefff
cemalfarukguney <cemalfarukguney@users.noreply.github.com>
2020-05-17 17:20:05 +0300

    Update ImageAdapter.java

--- Pictureit/app/src/main/java/com/example/pictureit/Utils/ImageAdapter.java
+++ Pictureit/app/src/main/java/com/example/pictureit/Utils/ImageAdapter.java
@@ -36,7 +36,7 @@ public class ImageAdapter extends ArrayAdapter<String>{
     }
 
     private static class ViewHolder{
-        ImageView image;
+        SquareImageView image;
         ProgressBar mProgressBar;
     }
 
@@ -52,7 +52,7 @@ public class ImageAdapter extends ArrayAdapter<String>{
             convertView = mInflater.inflate(layoutResource, parent, false);
             holder = new ViewHolder();
             holder.mProgressBar = (ProgressBar) convertView.findViewById(R.id.progressBar);
-            holder.image = (ImageView) convertView.findViewById(R.id.gridImageView);
+            holder.image = (SquareImageView) convertView.findViewById(R.id.gridImageView);
 
             convertView.setTag(holder);
         }
