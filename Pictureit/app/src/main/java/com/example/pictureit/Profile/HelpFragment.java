a980c124ae032f662ce56a9d68f6f5bb13ef23e6
İsmail Sergen Göçmen <ismailsergengocmen@users.noreply.github.com>
2020-05-19 11:55:46 +0300

    Launcher logo changed
    Ugly launcher logo replaced with a better one

--- Pictureit/app/src/main/java/com/example/pictureit/Profile/HelpFragment.java
+++ Pictureit/app/src/main/java/com/example/pictureit/Profile/HelpFragment.java
@@ -1,9 +1,12 @@
 package com.example.pictureit.Profile;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,10 +18,21 @@ public class HelpFragment extends Fragment {
 
     private static final String TAG = "HelpFragment";
 
+    //Variables
+    private TextView FirstP,SecondP,ThirdP,FourthP,FifthP;
+    private ImageView Questionmark;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_help, container, false);
+        FirstP = view.findViewById(R.id.FirstP);
+        SecondP = view.findViewById(R.id.SecondP);
+        ThirdP = view.findViewById(R.id.ThirdP);
+        FourthP = view.findViewById(R.id.FourthP);
+        FifthP = view.findViewById(R.id.FifthP);
+        Questionmark = view.findViewById(R.id.QuestionEmoji);
+
         return view;
     }
 }
