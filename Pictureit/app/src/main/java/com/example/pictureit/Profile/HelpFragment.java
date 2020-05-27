d081d2e7fa13d5f2c399674bac42e3bb29779a9a
İsmail Sergen Göçmen <ismailsergengocmen@users.noreply.github.com>
2020-05-27 18:50:02 +0300

    Revert "++"
    This reverts commit 8324539ace2d11c9c85e55adefd6bf8fee61d397.

--- Pictureit/app/src/main/java/com/example/pictureit/Profile/HelpFragment.java
+++ Pictureit/app/src/main/java/com/example/pictureit/Profile/HelpFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,19 +14,28 @@ import androidx.fragment.app.Fragment;
 
 import com.example.pictureit.R;
 
+
 public class HelpFragment extends Fragment {
 
     //Constants
     private static final String TAG = "HelpFragment";
 
     //Variables
-    private ImageView backArrow;
+    private TextView firstP, secondP, thirdP, fourthP, fifthP;
+    private ImageView questionmark, backArrow;
+
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_help, container, false);
+        firstP = view.findViewById(R.id.FirstP);
+        secondP = view.findViewById(R.id.SecondP);
+        thirdP = view.findViewById(R.id.ThirdP);
+        fourthP = view.findViewById(R.id.FourthP);
+        fifthP = view.findViewById(R.id.FifthP);
+        backArrow = view.findViewById(R.id.HelpbackArrow);
 
         backArrow.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +50,7 @@ public class HelpFragment extends Fragment {
     private void backToActivity() {
         getFragmentManager().popBackStack();
     }
+
 }
 
 
